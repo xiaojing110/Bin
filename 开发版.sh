8d4831026aa3f43e8db5dc2c38a38fa2cd39d112
xiaojing110 <62259239+xiaojing110@users.noreply.github.com>
2020-08-26 20:59:22 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #修改版本号
 #sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 
